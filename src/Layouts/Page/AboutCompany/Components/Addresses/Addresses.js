2d42fd01ea263db759f3c48a6fc9ac7640e5aa13
AnnetSmir <123948894+AnnetSmir@users.noreply.github.com>
2023-01-31 12:39:33 +0000

    исправлен блок с адресами

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,16 +1,27 @@
-import PrimaryButton from "../../../../../Components/PrimaryButton";
+import styled from "styled-components";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+
+const AddressesWrapper = styled.div`
+    max-width:453px;
+    min-height:420px;
+    border: 1px solid rgba(100, 116, 139, 0.12);
+    border-radius: 8px;
+    padding: 24px;
+`;
+
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+        <AddressesWrapper>
+            <div className='d-inline-flex flex-column w-100 h-100 gap-4'>
+                <div className='title'>Адреса заведений</div>
+                <div className='d-inline-flex flex-column col gap-2'>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                </div>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
-        </div>
+        </AddressesWrapper>
     );
 }
 
--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -7,15 +7,19 @@ import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
 const Option = styled.div`
     outline: none;
     border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
+    border: none;
+    background-color: none;
+    color: #0F172A;
 `;
-
+const OptionLeftWrapper = styled.div`
+    padding-right:12px;
+`;
 function ListElement(props) {
     return (
-        <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option className='flex-grow-1 p-2'>{props.children}</Option>
+        <div className='d-flex flex-row align-items-center align-self-stretch'>
+            <OptionLeftWrapper className='flex-grow-1'>
+                <Option className='pе=0 my-3 text-rg'>{props.children}</Option>   
+            </OptionLeftWrapper>
             <IconOnlyButton icon={<EditIcon/>}/>
             <IconOnlyButton icon={<RemoveIcon/>}/>
         </div>
